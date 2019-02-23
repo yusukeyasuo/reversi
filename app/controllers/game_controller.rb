7d72c1f2d6301496857cd90c506d931b33b4b6d9
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-02-23 17:52:50 +0900

    先行、後攻の初期値の修正

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -17,10 +17,10 @@ class GameController < ApplicationController
       
       # GameDetital 作成
       formation = Array.new(8).map{Array.new(8, nil)}
-      formation[3][3] = 1
-      formation[3][4] = 2
-      formation[4][3] = 2
-      formation[4][4] = 1
+      formation[3][3] = 2
+      formation[3][4] = 1
+      formation[4][3] = 1
+      formation[4][4] = 2
       game_detail = GameDetail.new(game_id: game.id, formation: formation.to_json)
       game_detail.save
       redirect_to action: :play, id: game.id and return
