b79c8f4fd91b404449dd101e8176efecf522390e
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-02-23 15:10:32 +0900

    ログインUIの調整

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,10 @@
 class GameController < ApplicationController
+    before_action :require_login
+    
     def index
     end
     
     def play
       @game_detail = GameDetail.find_by(game_id: params[:id])
-      pp @game_detail
     end
 end
